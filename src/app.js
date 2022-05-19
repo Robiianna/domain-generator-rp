1e39bc16da519a27db98da1b0659b4c33d8f3391
Robiianna <robidelaparra1@gmail.com>
2022-05-19 16:29:32 +0000

    :sparkles: terminado domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var pronouns = ["the", "our"];
+  var adjs = ["great", "big"];
+  var nouns = ["jogger", "racoon"];
+  var domains = [".com", ".net", ".io"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          console.log(pronoun + adj + noun + domain);
+        }
+      }
+    }
+  }
+
